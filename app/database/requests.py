d7e3a7c6b1b38e3a0180e93c45cab355fdb3f40d
oBserver <akbaralisharipov6@gmail.com>
2026-07-12 01:36:16 +0300

    requests
    Add function to add or retrieve a user from the database.

--- app/database/requests.py
+++ app/database/requests.py
@@ -1,1 +1,20 @@
+from app.database.madels import async_session
+from app.database.madels import User,Category,Item
+from sqlalchemy import select
+
+async def add_or_get_user(tg_id : int, first_name: str, username : str =None):
+  async with async_session() as session:
+    user = await session.scalar(select(User).where(User.tg_id == tg_id))
+    if not user:
+      user = User(tg_id=tg_id,
+                  first_name=first_name,
+                  username=username,
+                  tg_name = first_name)
+      session.add(user)
+      await session.commit()
+      
+      await session.refresh(user)
+    return user
+
+     
 
